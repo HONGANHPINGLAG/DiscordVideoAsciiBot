f86acf56fa2ca55e8d86a7d72501933342d47c2b
T <witiwtyler@gmail.com>
2024-05-15 20:48:21 -0700

    Formatting

--- Driver.py
+++ Driver.py
@@ -4,17 +4,17 @@ import ImageToAscii
 
 # link = input('Enter valid Youtube link here: ')
 link = 'https://youtu.be/9lNZ_Rnr7Jc?si=doHED9zHwVT4Y_Pf'
-# YoutubeToMp4.DownloadVideo(link)
+YoutubeToMp4.DownloadVideo(link)
 print('Finished Downloading')
 
-# ImageToAscii.FrameGrabber()
+ImageToAscii.FrameGrabber()
 print('Finished grabbing frames')
 
-# ImageToAscii.ImageResizer()
+ImageToAscii.ImageResizer()
 print('Finished resizing frames')
 
 ImageToAscii.FrameToAscii()
-print('Converted each frame to ascii')
+print('Converted frames to ascii')
 
 time.sleep(30)
 YoutubeToMp4.DeleteVideo()
