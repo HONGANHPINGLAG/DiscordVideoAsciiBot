5eb333c44d033eb2ce3748a16e34f1290fd34176
T <witiwtyler@gmail.com>
2024-05-17 01:30:30 -0700

    Created and finished file

---
+++ DeleteFlies.py
@@ -0,0 +1,42 @@
+import os
+import time
+       
+def deleteFiles(dirPath):
+    allframes = os.listdir(dirPath)
+    if len(allframes) > 0:
+        for eachFrame in allframes:
+            framePath = os.path.join(dirPath, eachFrame)
+            os.remove(framePath)
+
+def DeleteAllFiles():
+    print('\nDeleting all files')
+    
+    # Removing source video.mp4 file
+    videoPath = './mp4Files/video.mp4'
+    if os.path.isfile(videoPath): 
+        os.remove(videoPath)
+    
+    # Removing all of the files from the videoFrames, resizedFrames, and asciiFrames folders
+    framePaths = ['./videoFrames', './resizedFrames', './asciiFrames']
+    for path in framePaths:
+        deleteFiles(path)
+    
+    print('Successfully deleted all files\n')
+
+
+def DeleteVideoFile():
+    print('Removing video file\n')
+    os.remove('./mp4Files/video.mp4')
+
+def DeleteVideoFrames():
+    print('Removing video frames\n')
+    deleteFiles('./videoFrames')
+
+def DeleteResizedFrames():
+    print('Removing resized frames\n')
+    deleteFiles('./resizedFrames')
+
+def DeleteAsciiFrames():
+    print('Removing ascii frames\n')
+    deleteFiles('./asciiFrames')
+    
